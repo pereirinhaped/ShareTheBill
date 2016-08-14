ae22cc4f4f743bc29d5bd1cfcdf55f31c3b47376
Pedro Pereirinha <pereirinhaped@gmail.com>
2016-08-14 20:58:13 +0100

    String input limitation added: only numeric and , . accepted. Left zero cleared.

--- ShareTheBill/ShareTheBillVC.swift
+++ ShareTheBill/ShareTheBillVC.swift
@@ -47,6 +47,24 @@ class ShareTheBillVC: UIViewController, UITextFieldDelegate {
 		
 	}
 	
+	func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+		// Create an `NSCharacterSet` set which includes everything *but* the digits
+		let inverseSet = NSCharacterSet(charactersIn:"0123456789,.").inverted
+		
+		// At every character in this "inverseSet" contained in the string,
+		// split the string up into components which exclude the characters
+		// in this inverse set
+		let components = string.components(separatedBy: inverseSet)
+		
+		// Rejoin these components
+		let filtered = components.joined(separator: "")  // use join("", components) if you are using Swift 1.2
+		
+		// If the original string is equal to the filtered string, i.e. if no
+		// inverse characters were present to be eliminated, the input is valid
+		// and the statement returns true; else it returns false
+		return string == filtered
+	}
+	
 	func textFieldDidEndEditing(_ textField: UITextField) {
 		updateValues()
 	}
@@ -94,14 +112,23 @@ class ShareTheBillVC: UIViewController, UITextFieldDelegate {
 		
 		var billValueToString = ""
 		
+		// Left Zero cleaner
+		func removeZeros(aString: String) -> String {
+			if aString.characters.first != "0" {
+				return aString
+			}
+			return removeZeros(aString: aString.substring(from: aString.index(aString.startIndex, offsetBy: 1)))
+		}
+		
 		if let billValueTxt = billValueTxtFld.text {
 			if !billValueTxt.characters.contains(",") && !billValueTxt.characters.contains(".") {
-				billValueTxtFld.text = "\(billValueTxt),00"
+				billValueTxtFld.text = "\(removeZeros(aString: billValueTxt)),00"
 				billValueToString = billValueTxt
 			} else if billValueTxt.characters.contains(",") {
-				billValueToString = billValueTxt.replacingOccurrences(of: ",", with: ".")
+				billValueTxtFld.text = removeZeros(aString: billValueTxt)
+				billValueToString = removeZeros(aString: billValueTxt.replacingOccurrences(of: ",", with: "."))
 			} else {
-				billValueTxtFld.text = billValueTxt.replacingOccurrences(of: ".", with: ",")
+				billValueTxtFld.text = removeZeros(aString: billValueTxt.replacingOccurrences(of: ".", with: ","))
 				billValueToString = billValueTxt
 			}
 		}
@@ -130,7 +157,6 @@ class ShareTheBillVC: UIViewController, UITextFieldDelegate {
 			totalValueLbl.text = "\(totalValueTxt)"
 			eachSubTotalLbl.text = "\(eachSubTotalTxt)"
 		}
-		
 	}
 	
 	/*
